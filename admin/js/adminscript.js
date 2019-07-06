bcbe1b4d1805ce41bb3c0d6e79b4761b4df3ff84
amrit73 <amritxrestha75@gmail.com>
2019-07-06 01:51:03 -0300

    appointmentcontrol

--- admin/js/adminscript.js
+++ admin/js/adminscript.js
@@ -1,3 +1,67 @@
+$("#admin_appointment").submit(function(event) {
+    event.preventDefault();
+    var _form = $(this);
+    var postForm = { //Fetch form data
+        'token': localStorage.getItem("token"),
+        'id': localStorage.getItem("_id"),
+        'name': $('input[name=name]').val(),
+        'petname': $('input[name=petname]').val(),
+        'email': $('input[name=email]').val(),
+        'phone': $('input[name=phone]').val(),
+        'date': $('input[name=date]').val(),
+        'time': $('input[name=time]').val(),
+        'message': CKEDITOR.instances['editor2'].getData(),
+    };
+    $.ajax({
+        type: 'POST',
+        url: 'http://127.0.0.1:3000/api/admin/add_appointment',
+        data: postForm,
+        dataType: "json",
+        beforeSend: function() {
+            $(".main-btn").attr("disabled", true);
+        },
+        success: function(response) {
+            _form[0].reset();
+            CKEDITOR.instances['editor2'].setData('');
+            console.log(response);
+            $("#check").html(response.Success).addClass("alert");
+            $(".main-btn").attr("disabled", false);
+        },
+        error: function() {}
+    })
+});
+
+$("#admin_appointment_edit").submit(function(event) {
+    event.preventDefault();
+    var _form = $(this);
+    var postForm = { //Fetch form data
+        'token': localStorage.getItem("token"),
+        'id': localStorage.getItem("_id"),
+        '_id': $('input[name=_id]').val(),
+        'name': $('input[name=name]').val(),
+        'petname': $('input[name=petname]').val(),
+        'email': $('input[name=email]').val(),
+        'phone': $('input[name=phone]').val(),
+        'date': $('input[name=date]').val(),
+        'time': $('input[name=time]').val(),
+        'message': CKEDITOR.instances['editor2'].getData(),
+    };
+    $.ajax({
+        type: 'POST',
+        url: 'http://127.0.0.1:3000/api/admin/update_appointment',
+        data: postForm,
+        dataType: "json",
+        beforeSend: function() {
+            $(".main-btn").attr("disabled", true);
+        },
+        success: function(response) {
+            $("#check").html(response.Success).addClass("alert");
+            $(".main-btn").attr("disabled", false);
+        },
+        error: function() {}
+    })
+});
+
 $("#admin_user").submit(function(event) {
     event.preventDefault();
     var _form = $(this);
